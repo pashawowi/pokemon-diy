b27f5008b6d0515dbfd3bb4208ccfcb8c5065927
zhuang18 <zhuang18@purdue.edu>
2016-08-19 04:10:36 -0400

    create pokemon.model.scala

--- app/models/Pokemon.scala
+++ app/models/Pokemon.scala
@@ -1,8 +1,40 @@
 package models
 
+import models.Type.Type
+import play.api.libs.json._
 /**
   * Created by theo on 8/19/16.
   */
-class Pokemon {
 
-}
+
+case class Pokemon(
+  id: Int,
+  chinese_name: String,
+  english_name: String,
+  description: Option[String],
+  type_1: Type,
+  type_2: Option[Type],
+  ability_1: Int,
+  ability_2: Option[Int],
+  ability_3: Option[Int],
+  full_exp: Int,
+  height: Double,
+  weight: Double,
+  male_rate: Double,
+  catch_rate: Int,
+  birth_period: Int,
+  base_stat: JsValue,
+  base_exp: Int,
+  species_hp: Int,
+  species_attack: Int,
+  species_defense: Int,
+  species_sa: Int,
+  species_sd: Int,
+  species_speed: Int,
+  evolve_from: Option[Int],
+  evolve_to: Option[Int],
+  evolve_item: Option[Int],
+  learnable_move: JsValue,
+  useable_tm: JsValue,
+  inherit_move: JsValue
+)
